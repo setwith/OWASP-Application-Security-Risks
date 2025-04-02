f29655553ad377c365160a4c7b38acc3c1876641
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2025-04-03 00:02:41 +0300

    A10:2021 - Server-Side Request Forgery (SSRF)

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -2,6 +2,7 @@ class ProxyController < ApplicationController
   require 'net/http'
   
   # A04:2021 - Insecure Design. Lack of proper security design, allows unlimited requests without resource validation
+  # A10:2021 - Server-Side Request Forgery (SSRF). Allows requests to internal network, Missing URL validation
   def fetch
     url = params[:url]
     response = Net::HTTP.get(URI(url))
